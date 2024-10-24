3e51a106bbb758557a2ed7317c4212a712fd733e
vadkvadrad <vadik.kudrv@yandex.ru>
2024-10-24 16:48:07 +0300

    обновлен auth

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,1 +1,110 @@
 package auth
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log/slog"
+	"sso/internal/domain/models"
+	"time"
+)
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+// New returns new instance of the Auth service
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userProvider: userProvider,
+		userSaver:    userSaver,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+func (a *Auth) Login(
+	ctx context.Context,
+	email string,
+	password string,
+	appID int,
+) (string, error) {
+	const op = "auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+		slog.String("appID", fmt.Sprint(appID)))
+
+	log.Info("attempting to login user")
+
+	user, err := a.userProvider.User(ctx, email)
+	if err != nil {
+		log.Error("failed to find user", err.Error())
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+}
+
+func (a *Auth) RegisterNewUser(
+	ctx context.Context,
+	email string,
+	password string,
+) (int64, error) {
+	const op = "auth.RegisterNewUser"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email))
+
+	log.Info("registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		log.Error("failed to hash password", err.Error())
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	var id int64
+	id, err = a.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user", err.Error())
+		return 0, fmt.Errorf("%s, %w", op, err)
+	}
+
+	log.Info("successfully saved user")
+	return id, nil
+}
+
+func (a *Auth) login(
+	ctx context.Context,
+	userID int64,
+) (bool, error) {
+	panic("implement me")
+}
